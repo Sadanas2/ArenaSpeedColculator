4ccc90b71e53ea8e2d3ed07742a104bf54937b4d
Tomas <tomas.kubilius.93@gmail.com>
2022-02-07 17:23:16 +0200

    Add TextView in SavedTeams

--- app/src/main/java/lt/vcs/arenaspeedcolculator/SavedTeams.java
+++ app/src/main/java/lt/vcs/arenaspeedcolculator/SavedTeams.java
@@ -2,6 +2,7 @@ package lt.vcs.arenaspeedcolculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,10 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SavedTeams extends MainActivity {
 
+    private TextView chamsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        chamsList= findViewById(R.id.chamsList);
+//        noteContentEditText
     }
 }
