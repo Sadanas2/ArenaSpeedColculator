40d4290b8740d9fa5573416a584b2ba39ccebf6f
Tomas <tomas.kubilius.93@gmail.com>
2022-02-08 20:59:05 +0200

    Set up champions buttons

--- app/src/main/java/lt/vcs/arenaspeedcolculator/NextActivity.java
+++ app/src/main/java/lt/vcs/arenaspeedcolculator/NextActivity.java
@@ -1,19 +1,70 @@
 package lt.vcs.arenaspeedcolculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class NextActivity extends MainActivity {
+    private Button mainchampbutton;
+    private Button secondbchampbutton;
+    private Button thirdchampbutton;
+    private Button fourthchampbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        mainchampbutton = findViewById(R.id.mainchampbutton);
+        secondbchampbutton = findViewById(R.id.secondbchampbutton);
+        thirdchampbutton = findViewById(R.id.thirdchampbutton);
+        fourthchampbutton = findViewById(R.id.fourthchampbutton);
+
+        setUpMainChampButton();
+        setUpSecondChampionButton();
+        setUpThirdChampionButton();
+        setUpfourthChampionButton();
+
 
     }
-
-
+    private void setUpMainChampButton() {
+        mainchampbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NextActivity.this, ChampionsWindow.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void setUpSecondChampionButton() {
+        secondbchampbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NextActivity.this, ChampionsWindow.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void setUpThirdChampionButton() {
+        thirdchampbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NextActivity.this, ChampionsWindow.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void setUpfourthChampionButton() {
+        fourthchampbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NextActivity.this, ChampionsWindow.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
