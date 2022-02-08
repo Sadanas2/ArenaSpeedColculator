d3044cae18453ffe8d43063a16d82c69b21595b7
Tomas <tomas.kubilius.93@gmail.com>
2022-02-08 19:53:00 +0200

    Add Query and Insert in ChampDao

--- app/src/main/java/lt/vcs/arenaspeedcolculator/ChampDao.java
+++ app/src/main/java/lt/vcs/arenaspeedcolculator/ChampDao.java
@@ -1,16 +1,31 @@
 package lt.vcs.arenaspeedcolculator;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 @Dao
 public interface ChampDao {
 
-    @Query("SELECT * FROM champs")
+    @Query("SELECT * FROM champss")
     List<Champ> getAll();
 
-    @Query("SELECT * FROM champs WHERE id IN (:userIds)")
+    @Query("SELECT * FROM champss WHERE id IN (:userIds)")
     List<Champ> loadAllByIds(int[] userIds);
+
+
+    @Query("SELECT * FROM " + "champss" + " WHERE id =:id")
+    Champ getItem(int id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertChamps(List<Champ> champslist);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertChamp(Champ champs);
+
+    @Query("DELETE FROM " + "champss" + " WHERE id =:id")
+    void deleteItem(int id);
 
 }
