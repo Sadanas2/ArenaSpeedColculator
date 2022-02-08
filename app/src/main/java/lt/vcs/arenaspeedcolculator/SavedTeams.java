b041774a30fa60ae9a1e056810decd9701a64712
Tomas <tomas.kubilius.93@gmail.com>
2022-02-08 20:33:19 +0200

    Set up SavedTeams

--- app/src/main/java/lt/vcs/arenaspeedcolculator/SavedTeams.java
+++ app/src/main/java/lt/vcs/arenaspeedcolculator/SavedTeams.java
@@ -7,6 +7,8 @@ public class SavedTeams extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_list);
+
+
     }
 }
