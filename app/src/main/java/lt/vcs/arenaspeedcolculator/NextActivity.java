f18e3626626b8b10f19ad1bc519d7ef7d05a699f
Tomas <tomas.kubilius.93@gmail.com>
2022-02-09 13:42:41 +0200

    add all buttons in seUpUi

--- app/src/main/java/lt/vcs/arenaspeedcolculator/NextActivity.java
+++ app/src/main/java/lt/vcs/arenaspeedcolculator/NextActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import lt.vcs.arenaspeedcolculator.champsrepository.ChampionsWindow;
 
 public class NextActivity extends MainActivity {
-    protected Button mainChampButton, secondbChampButton, thirdChampButton, fourthChampButton,firstChampAllSpeed;
+    protected Button mainChampButton, secondbChampButton, thirdChampButton, fourthChampButton,firstChampAllSpeed,saveButton, secondChampNeededSpeedButton,thirdChampNeededSpeedButton,fourthChampNeededSpeedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,7 @@ public class NextActivity extends MainActivity {
         setUpSecondChampionButton();
         setUpThirdChampionButton();
         setUpfourthChampionButton();
+        setUpSaveButton();
 
 
     }
@@ -72,12 +73,21 @@ public class NextActivity extends MainActivity {
             }
         });
     }
+
+    private void setUpSaveButton(){
+
+    }
+
     private void setUpUi(){
         mainChampButton = findViewById(R.id.mainChampButton);
         secondbChampButton = findViewById(R.id.secondChampButton);
         thirdChampButton = findViewById(R.id.thirdChampButton);
         fourthChampButton = findViewById(R.id.fourthChampButton);
-        firstChampAllSpeed = findViewById(R.id.fourthChampButton);
+        firstChampAllSpeed = findViewById(R.id.firstChampAllSpeed);
+        saveButton = findViewById(R.id.saveButton);
+        secondChampNeededSpeedButton = findViewById(R.id.secondChampNeededSpeedButton);
+        thirdChampNeededSpeedButton = findViewById(R.id.thirdChampNeededSpeedButton);
+        fourthChampNeededSpeedButton = findViewById(R.id.fourthChampNeededSpeedButton);
 
     }
 }
